0cf8d34f375ddb2df6d477da2e3e174001952c8b
Torchon14 <119144258+Torchon14@users.noreply.github.com>
2022-11-27 15:10:19 +0100

    Add files via upload

--- Assesment_function.py
+++ Assesment_function.py
@@ -1,4 +1,4 @@
-v1=0; v2 = 0; v3 = 0; v4 = 0; v5 = 0;Premium=0;v7 =0; typePolicy = ["Car Insurance","Home Insurance","Mobile Phone Insurance"]; methodPayment = ["Bank transfer","Paypal","Credit Card"]
+v1=0; v2 = 0; v3 = 0; v4 = 0;Kind= 0; v5 = 0;Premium=0;v7 =0; typePolicy = ["Car Insurance","Home Insurance","Mobile Phone Insurance"]; methodPayment = ["Bank transfer","Paypal","Credit Card"]; fee = 0
 
 
 def Welcome(v1):
@@ -14,44 +14,46 @@ def fullName(v2) :
         v2 = input("Please insert your full name: \n")
 def policyDuration(v3):
     v3 = input("Please insert the requested duration of your policy (in years):\n")
-    #float(v3)
     while  not v3.isnumeric():
         print("Incorrect ! Please enter a correct number (it needs to be an integer)")
         v3 = input("Please insert the requested duration of your policy (in years):\n")
-def policyType(Premium):
-    
-   for type in typePolicy:
+    if v3.isnumeric():
+        v3 = int(v3)
+        return v3
+def policyType(v4):
+     for type in typePolicy:
         print("Do you wish to select",type,"?")
         v4 = input ("Yes or No ?\n")
         if v4 == "Yes" or v4 == "yes":
             print("You have chosen", type, "as your policy")
-            if type == typePolicy[0]:              
-                Premium == 500
-                break
-            elif type == typePolicy[1]:
-                v6 += 1000
-                break
-            elif type == typePolicy[2]:
-                v6 += 120
-                break 
-        else:
-            continue 
-
-        
-def paymentMethod(v5):
+            return type 
+def policyPremium(kind,Premium,v3):
+    if kind == typePolicy[0]:              
+                Premium += 500*v3
+                return Premium
+    elif kind == typePolicy[1]:
+                Premium += 1000*v3
+                return Premium
+    elif kind == typePolicy[2]:
+                Premium += 120*v3
+                return Premium       
+def paymentMethod(fee):
     for method in methodPayment:
         print("Do you wish to select",method,"?")
         v5 = input ("Yes or No ?\n")
         if v5 == "Yes" or v5 == "yes":
             v5 = method
             print("You have chosen", method, "as your payment method")
-            print(v5)
-            break       
+            if method == methodPayment[0]:              
+                fee = 0
+                return fee
+            elif method == methodPayment[1]:
+                fee = 0.10
+                return fee
+            elif method == methodPayment[2]:
+                fee = 0.20
+                return fee 
         else: 
             continue
 
-  
-
-policyType(0)      
-print(Premium)
 
--- Assesment_prog.py
+++ Assesment_prog.py
@@ -1,6 +1,6 @@
 import Assesment_function
 #variables
-v1= 0;v2 = 0; v3 = 0; v4 = 0; v5 = 0
+v1= 0;v2 = 0; v3 = 0; v4 = 0; v5 = 0; Kind = 0;Premium = 0; fee = 0; Price =0
 
 #Program
 
@@ -8,11 +8,20 @@ print('Welcome to Admiral Insurance')
 Assesment_function.Welcome(v1)
 print("Esteemed customer ! It is time to establish your profile !")
 Assesment_function.fullName(v2)
-Assesment_function.policyDuration(v3)
-Assesment_function.policyType(v4)
-Assesment_function.paymentMethod(v5)
-print("You have completed your profile ! Esteemed customer, please see how much your selected policy will cost")
+v3 = Assesment_function.policyDuration(v3)
 
-
+Kind = Assesment_function.policyType(v4)
+Premium = Assesment_function.policyPremium(Kind,Premium,v3)
+fee = Assesment_function.paymentMethod(fee)
+print()
+print("You have completed your profile !")
+Price = Premium*fee + Premium
+print()
+print(" Dear customer, you asked for a", v3,"years",Kind,"contract. The total cost is of £",Price)
+print()
 
    
+# à améliorer = FullName verification,
+#               mixing Policy, 
+#               adding sentences during the profil establishment
+#               Allow user to choose in months to respect the phone Insurance policy
